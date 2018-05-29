fdc2d8772b7118393467e9107c8fefd057e6a77a
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-05-29 07:06:49 +0200

    Adds icons for status

--- templates/helpers/resolveAgreementStatus.js
+++ templates/helpers/resolveAgreementStatus.js
@@ -1,1 +1,9 @@
-module.exports = input => !input ? 'unknown' : input.status
+const translateStatus = {
+  cancelled: '<i class="material-icons">cancel</i> Avbrutt',
+  expired: '<i class="material-icons">watch_later</i> Utløpt',
+  signed: '<i class="material-icons">check_circle</i> Signert',
+  unknown: '<i class="material-icons">contact_support</i> Ukjent',
+  unsigned: '<i class="material-icons">error</i> Usignert'
+}
+
+module.exports = input => !input ? '<i class="material-icons">contact_support</i> Ukjent' : translateStatus(input.status)
