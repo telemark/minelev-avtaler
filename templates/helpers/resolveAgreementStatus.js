d29f4b7fdd37c644fde2ae1663ed32834b8e519a
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-05-29 07:19:04 +0200

    Adds color to icons

--- templates/helpers/resolveAgreementStatus.js
+++ templates/helpers/resolveAgreementStatus.js
@@ -1,9 +1,9 @@
 const translateStatus = {
-  cancelled: '<i class="material-icons">cancel</i> Avbrutt',
-  expired: '<i class="material-icons">watch_later</i> Utløpt',
-  signed: '<i class="material-icons">check_circle</i> Signert',
-  unknown: '<i class="material-icons">contact_support</i> Ukjent',
-  unsigned: '<i class="material-icons">error</i> Usignert'
+  cancelled: '<i class="text-warning material-icons">cancel</i> Avbrutt',
+  expired: '<i class="text-warning material-icons">watch_later</i> Utløpt',
+  signed: '<i class="text-success material-icons">check_circle</i> Signert',
+  unknown: '<i class="text-unknown material-icons">contact_support</i> Ukjent',
+  unsigned: '<i class="text-unknown material-icons">error</i> Usignert'
 }
 
 module.exports = input => !input ? '<i class="material-icons">contact_support</i> Ukjent' : translateStatus(input.status)
