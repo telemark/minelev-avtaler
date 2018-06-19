ac47c6f6b1f7d7fe0003b25e840ebb7a34872608
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-06-19 12:51:24 +0200

    Implements download of agreements

--- handlers/agreements.js
+++ handlers/agreements.js
@@ -1,9 +1,13 @@
 const { pack, unpack } = require('jcb64')
+const uuid = require('uuid/v4')
+const os = require('os')
+const fs = require('fs')
 const getStudents = require('../lib/get-students-in-class')
 const getAgreements = require('../lib/get-agreements-for-students')
 const getAgreement = require('../lib/get-agreement-details')
 const logger = require('../lib/logger')
 const createViewOptions = require('../lib/create-view-options')
+const generateExcelFile = require('../lib/generate-excel-file')
 const isValidAgreement = agreement => ['elevpc', 'boker', 'images'].includes(agreement.agreementType)
 
 function getAgreementStatus (agreement) {
@@ -29,6 +33,76 @@ function nameSort (a, b) {
     num = 1
   }
   return num
+}
+
+function filterFields (data) {
+  return {
+    Etternavn: data.lastName,
+    Fornavn: data.firstName,
+    'ElevPC': data.elevpc,
+    'Bøker': data.boker,
+    'Bilder': data.images,
+    'E-post': data.mail,
+    Mobiltelefon: data.mobilePhone
+  }
+}
+
+module.exports.downloadAgreements = async (request, h) => {
+  const userId = request.auth.credentials.data.userId
+  const classId = request.params.classID
+  const uniqueName = `${classId}-${uuid()}.xlsx`
+  const filename = `${os.tmpdir()}/${uniqueName}`
+
+  logger('info', ['agreements', 'downloadAgreements', 'classId', classId, 'userId', userId])
+
+  const students = await getStudents({
+    userId: userId,
+    classId: classId
+  })
+
+  students.sort(nameSort)
+
+  const agreements = await getAgreements({
+    userId: userId,
+    students: students
+  })
+
+  const groupedAgreements = agreements.reduce((prev, current) => {
+    if (!prev.hasOwnProperty(current.agreementId)) {
+      prev[current.agreementId] = Object.assign({}, current, {parts: []})
+    }
+    prev[current.agreementId].parts.push(current)
+    return prev
+  }, {})
+
+  const validAgreements = Object.values(groupedAgreements).map(agreement => Object.assign({}, agreement, {signs: agreement.parts.map(a => a.status)})).filter(isValidAgreement)
+
+  const repackedAgreements = validAgreements.reduce((prev, curr) => {
+    if (!prev.hasOwnProperty(curr.agreementUserId)) {
+      prev[curr.agreementUserId] = {}
+    }
+    prev[curr.agreementUserId][curr.agreementType] = getAgreementStatus(curr)
+    return prev
+  }, {})
+
+  const repackedStudents = students.map(student => Object.assign({}, student, repackedAgreements[student.personalIdNumber])).map(filterFields)
+
+  await generateExcelFile({filename: filename, data: repackedStudents})
+  const excel = fs.readFileSync(filename)
+
+  request.raw.req.once('end', () => {
+    logger('info', ['agreements', 'downloadAgreements', 'user', userId, uniqueName, 'success'])
+    try {
+      fs.unlinkSync(filename)
+      logger('info', ['agreements', 'downloadAgreements', 'user', userId, uniqueName, 'cleanup finished'])
+    } catch (error) {
+      logger('error', ['agreements', 'downloadAgreements', 'user', userId, 'unlink', error])
+    }
+  })
+
+  return h.response(excel)
+    .header('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    .header('Content-Disposition', 'attachment; filename=' + uniqueName)
 }
 
 module.exports.getAgreements = async (request, h) => {
