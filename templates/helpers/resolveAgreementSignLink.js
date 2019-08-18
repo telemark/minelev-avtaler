443306a27bd5ddb3b2ca6945ce83a1dba7333574
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-08-18 19:09:13 +0200

    Removes link for signing if manual agreement (patch)

--- templates/helpers/resolveAgreementSignLink.js
+++ templates/helpers/resolveAgreementSignLink.js
@@ -1,6 +1,6 @@
 module.exports = agreement => {
   let message = 'Finner ikke url for signering. Er dette et manuelt oppdrag?'
-  if (agreement.status !== 'signed') {
+  if (agreement.status !== 'signed' && agreement.isManual === false) {
     message = `https://svarut.ks.no/forsendelse/${agreement.partId}/1/signering`
   } else if (agreement.status === 'signed') {
     message = 'oppdraget er allerede signert'
@@ -8,6 +8,8 @@ module.exports = agreement => {
     message = 'Oppdraget skal ikke signeres'
   } else if (agreement.agreementType === 'images') {
     message = 'Be eleven logge seg inn på https://meg.minelev.no'
+  } else if (agreement.isManual === true) {
+    message = 'Denne avtalen må håndteres manuelt. Be eleven logge seg inn på https://meg.minelev.no dersom avtale må lastes ned for signering.'
   }
   return message
 }
